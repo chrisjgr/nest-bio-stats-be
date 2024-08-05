c1fc12dbad01074549ce641708cf137bdd1c3276
Christian <chrisdb150@gmail.com>
2024-08-04 22:41:36 -0400

    creating auth config

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,9 +2,10 @@ import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from 'src/config/config.module';
+import { AuthModule } from 'src/authentication/auth.module';
 
 @Module({
-  imports: [ConfigModule],
+  imports: [ConfigModule, AuthModule],
   controllers: [AppController],
   providers: [AppService],
 })
--- test/app.e2e-spec.ts
+++ test/app.e2e-spec.ts
@@ -21,4 +21,3 @@ describe('AppController (e2e)', () => {
       .expect('Hello World!');
   });
 });
-
